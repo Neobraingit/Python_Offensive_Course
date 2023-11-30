d7540fb8d681b6a297af41a31c4ad4d794e810f3
Neobraingit <Maervca2@icloud.com>
2023-11-30 15:57:37 +0100

    Flota_vehículos

--- Proyectos_POO/Flota_vehículos.py
+++ Proyectos_POO/Flota_vehículos.py
@@ -5,13 +5,28 @@ class Vehiculo:
         self.matricula = matricula
         self.modelo = modelo
         self.disponible = True
-
+        
+    def alquilar(self):
+        if self.disponible:
+            self.disponible = False
+        else:
+            print (f' El vehículo con matrícula {self.matricula} no se puede alquilar')
+            
+            
+    def __str__(self):
+        return f'Vehiculo(matricula = {self.matricula}, modelo = {self.modelo}, disponible = {self.disponible})'
 class Flota:
     def __init__(self):
         self.vehiculos = []
         
     def agregar_vehiculo(self, vehiculo):
         self.vehiculos.append(vehiculo)
+        
+    def alquilar_vehiculo(self, matricula):
+        for vehiculo in self.vehiculos:
+            if vehiculo.matricula == matricula:
+                vehiculo.alquilar()
+                
         
     def __str__(self):
         return '\n'.join(str(vehiculo for vehiculo in self.vehiculos))
@@ -23,4 +38,11 @@ if __name__ == '__main__':
     flota.agregar_vehiculo(Vehiculo('5898MJZ', 'Toyota Corolla'))
     flota.agregar_vehiculo(Vehiculo('1234MJZ', 'Honda Civic'))
     
-print (f'[+] Flota inicial {flota}')
+print ('[+] Flota inicial\n')
+print (flota)
+
+print ('\nMostrando la flota después de haber alquilado el Toyota:\n')
+print (flota)
+
+
+print (flota)
