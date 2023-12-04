339fc1ac47b08ee3f1d6b2cf874fc8e36465e4c0
Neobraingit <Maervca2@icloud.com>
2023-12-04 21:12:22 +0100

    Scanner_Port

--- Introducción_python_ofensivo/Escaner_puertos.py
+++ Introducción_python_ofensivo/Escaner_puertos.py
@@ -2,21 +2,43 @@
 #!/usr/bin/env python3
 
 import socket
+import argparse
+import sys
+from termcolor import colored
 
-host = '192.168.1.1'
-port = 80
 
+def get_arguments():
+    parser = argparse.ArgumentParser(description = 'Fast TCP Port Scanner')
+    parser.add_argument('-t', '--target', dest='target', help='Victim target to scan (Ex: -t 192.168.1.1)')
+    options = parser.parse_args()
+    
+    if not options.target:
+        print (colored(f'\n[!] No se ha proporcionado el target', 'red'))
+        sys.exit(1)
+        
+    return options.target
 
-def port_scanner():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if s.connect_ex((host, port)):
-        print (f'\n[+] El puerto {port} está abierto.')
-    else:
-        print (f'\n[!] El puerto {port} está cerrado.')
+def create_socket():
+     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     s.settimeout(1)
+     return s
     
 
+def port_scanner(port, host, s):
+    try:
+        s.connect((host, port))
+        print (colored(f'\n[+] El puerto {port} está abierto¡', 'red'))
+        s.close()
+    except (socket.timeout, ConnectionRefusedError):
+        pass
+        
+    
 def main():
-    port_scanner(port)
+    target = get_arguments()
+    for port in range(1, 1000):
+        s = create_socket()
+        port_scanner(port, target, s)
+    
     
 if __name__ == '__main__':
     main()
