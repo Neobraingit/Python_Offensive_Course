d546bd9f311ea6a2177ae680f37e2b84b519c362
Neobraingit <Maervca2@icloud.com>
2023-12-06 12:58:14 +0100

    Port_scanner

--- Introducción_python_ofensivo/Escaner_puertos.py
+++ Introducción_python_ofensivo/Escaner_puertos.py
@@ -4,16 +4,18 @@
 import socket
 import argparse
 import sys
+import pdb
 from termcolor import colored
 
 
 def get_arguments():
     parser = argparse.ArgumentParser(description = 'Fast TCP Port Scanner')
     parser.add_argument('-t', '--target', dest='target', help='Victim target to scan (Ex: -t 192.168.1.1)')
+    parser.add_argument('-p', '--port', dest='port', help='Port range to scan (Ex: -p 1-100)')
     options = parser.parse_args()
     
     if not options.target:
-        print (colored(f'\n[!] No se ha proporcionado el target', 'red'))
+        parser.print_help()
         sys.exit(1)
         
     return options.target
@@ -27,17 +29,30 @@ def create_socket():
 def port_scanner(port, host, s):
     try:
         s.connect((host, port))
-        print (colored(f'\n[+] El puerto {port} está abierto¡', 'red'))
+        print (colored(f'\n[+] El puerto {port} está abierto¡', 'green'))
         s.close()
     except (socket.timeout, ConnectionRefusedError):
         pass
         
     
 def main():
-    target = get_arguments()
-    for port in range(1, 1000):
+    target, port = get_arguments()
+    if '-' in port:
+        ports = port.split('-')
+        pdb.set_trace()
+    elif ',' in port:
+        ports = port.split(',')
+        
+    for port in range(int(ports[0]), int(ports[1])):
         s = create_socket()
         port_scanner(port, target, s)
+        
+    for port in ports:
+        s = create_socket()
+        port_scanner(port, target, s)
+        
+   
+    
     
     
 if __name__ == '__main__':
